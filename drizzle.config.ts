a96b4d477f63290a3b30d5c6021d6f82e842a7a1
Wade Fletcher <3798059+wadefletch@users.noreply.github.com>
2023-09-16 11:59:55 -0700

    tweak drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,9 @@ import type { Config } from "drizzle-kit";
 import "dotenv/config";
 
 export default {
-  schema: "./src/lib/db/schema.ts",
-  out: "./src/lib/db/migrations",
+  schema: "./lib/db/schema.ts",
+  out: "./lib/db/migrations",
+  driver: "mysql2",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
